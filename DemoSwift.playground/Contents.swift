a1a4dc55fb4fb90abb22122bde750e55953630e0
cknguyenfr <can.khac.nguyen@framgia.com>
2018-07-05 13:37:54 +0700

    5/7-SwiftTraining-ques1-Optional update

--- DemoSwift.playground/Contents.swift
+++ DemoSwift.playground/Contents.swift
@@ -1,7 +1,5 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
-
 //1.1
 let maximumOfPlayer = 10
 //Because maximumOfPlayer is a constant and is declared by "let"
@@ -10,6 +8,7 @@ let maximumOfPlayer = 10
 //maximumOfPlayer = 20 >> error
 var currentPlayer : Int?
 print("aa: \(String(describing: currentPlayer))")
+print("================================================")
 //================================================
 
 //1.2 Tuple
@@ -20,9 +19,27 @@ let someone = (name: "Kane", age: 24)
 print("the error code: \(httpNotFoundError.0) and its description: \(httpNotFoundError.1)")
 print("the error code: \(errorCode) and its description: \(errorDes)")
 print("someone's name: \(someone.name) and age: \(someone.age) years old")
+print("================================================")
+//================================================
 
 //1.3 Optional
 //a simple Optional with default value
-var nonOptionValue : Int? = 100
-print("value: \(nonOptionValue ?? 1)")
-nonOptionValue = nil
+var optionValue : Int? = 100
+print("value: \(optionValue ?? 1)")
+optionValue = nil
+//when to use ! and ?
+let nonOptionalValue : String!
+nonOptionalValue = "this is non optional value"
+print(nonOptionalValue)
+//because of result of max function is optional
+var maxOfArray : Any?
+var arrayInt = [1, 2, 3]
+maxOfArray = arrayInt.max()
+print("value = \(maxOfArray)")
+arrayInt.removeAll()
+maxOfArray = arrayInt.max()
+print("value after remove all = \(maxOfArray)")
+print("================================================")
+//================================================
+
+
